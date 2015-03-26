db0afff80553087c143b1941b0770e30e48a2829
camunity <cameron.flowers13@gmail.com>
2015-03-26 15:15:41 -0500

    organizers and helper class

---
+++ FavoritePhotos/FPDataManager.h
@@ -0,0 +1,13 @@
+//
+//  FPDataManager.h
+//  FavoritePhotos
+//
+//  Created by Cameron Flowers on 3/26/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FPDataManager : NSObject
+
+@end
---
+++ FavoritePhotos/FPDataManager.m
@@ -0,0 +1,13 @@
+//
+//  FPDataManager.m
+//  FavoritePhotos
+//
+//  Created by Cameron Flowers on 3/26/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import "FPDataManager.h"
+
+@implementation FPDataManager
+
+@end
