c2b6a0eadbb27d542530c3cc2c1b511a71270e1b
its-micah <mlanier86@gmail.com>
2015-03-26 15:29:04 -0500

    set up JSON call

--- FavoritePhotos/ViewController.m
+++ FavoritePhotos/ViewController.m
@@ -10,8 +10,12 @@
 
 @interface ViewController ()
 
-@property NSDictionary *instaDictionary;
-@property NSArray *foundPicturesArray;
+@property NSDictionary *masterDictionary;
+@property NSArray *differentPosts;
+@property NSDictionary *individualPosts;
+@property NSDictionary *allPicsOnPosts;
+@property NSDictionary *almostThere;
+@property NSMutableArray *pictureArray;
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionView;
 
 @end
@@ -20,9 +24,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
+    self.pictureArray = [NSMutableArray new];
+    [self fillUpTheArray:@"aliveandfreefilm"];
+    NSLog(@"%li", self.pictureArray.count);
 
 }
+
+
+-(void)fillUpTheArray:(NSString *)whatkindoftag
+{
+    NSString *tempstring = [NSString stringWithFormat:@"https://api.instagram.com/v1/tags/%@/media/recent?access_token=414285079.1fb234f.995c050432af47ebbf899f824d393580", whatkindoftag];
+    NSURL *url = [NSURL URLWithString:tempstring];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+
+    [NSURLConnection sendAsynchronousRequest:request
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response,
+                                               NSData *data,
+                                               NSError *connectionError)
+     {
+         self.masterDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&connectionError];
+         self.differentPosts = [self.masterDictionary objectForKey:@"data"];
+         for (NSDictionary *tempDic in self.differentPosts)
+         {
+             self.allPicsOnPosts = [tempDic objectForKey:@"images"];
+             self.almostThere = [self.allPicsOnPosts objectForKey:@"standard_resolution"];
+             NSString *pictureURL = [self.almostThere objectForKey:@"url"];
+             [self.pictureArray addObject:pictureURL];
+             NSLog(@"%li", self.pictureArray.count);
+         }
+     }];
+}
+
 
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
@@ -32,8 +65,16 @@
 }
 
  - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-     return  self.foundPicturesArray.count;
+     return  self.pictureArray.count;
  }
+
+- (void)setUpCollectionView {
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    [flowLayout setScrollDirection:UICollectionViewScrollDirectionHorizontal];
+    [flowLayout setMinimumLineSpacing:0.5];
+    [self.collectionView setPagingEnabled:YES];
+    [self.collectionView setCollectionViewLayout:flowLayout];
+}
 
 
 
