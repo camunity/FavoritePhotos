5eea4d2f556be394fc4c0e382640ad78c1c3081e
camunity <cameron.flowers13@gmail.com>
2015-03-26 14:21:37 -0500

    first commit

---
+++ FavoritePhotos/FavoritePhotosViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritePhotosViewController.h
+//  FavoritePhotos
+//
+//  Created by Cameron Flowers on 3/26/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritePhotosViewController : UIViewController
+
+@end
